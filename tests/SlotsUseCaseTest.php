b8d9c6caf0c43056d8a821aa5583514e408f81d3
Lech Sawon <lech.sawon@docplanner.com>
2020-11-23 16:06:38 +0100

    add namespaces

--- tests/SlotsUseCaseTest.php
+++ tests/SlotsUseCaseTest.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
-final class DoctorUseCaseTest extends E2ETestCase
+final class SlotsUseCaseTest extends E2ETestCase
 {
     public function test() {
         $client = static::createClient();
 
-        $client->request('GET', '/doctor?id=999');
+        $client->request('GET', '/slots/999');
 
         $this->assertEquals('404', $client->getResponse()->getStatusCode());
     }
