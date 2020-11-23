f886f92ca144ca13062466c12835e87e547fbe6b
Lech Sawon <lech.sawon@docplanner.com>
2020-11-23 15:45:01 +0100

    install phpunit

---
+++ tests/DoctorUseCaseTest.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class DoctorUseCaseTest extends WebTestCase
+{
+    public function test() {
+        die('asfasdfasdf');
+    }
+}
