860017032bb246dd44969961da5d12ec208514f4
Tomasz Ksionek <tomasz.ksionek@docplanner.com>
2021-02-15 10:18:14 +0100

    codebase

--- tests/DoctorSlotsUseCaseTest.php
+++ tests/DoctorSlotsUseCaseTest.php
@@ -5,20 +5,96 @@ namespace App\Tests;
 
 final class DoctorSlotsUseCaseTest extends E2ETestCase
 {
+    private CONST EXPECTED_CREATE_RESPONSE = '{"id":1}';
+    private CONST EXPECTED_GET_RESPONSE = '{"id":1,"firstName":"John","lastName":"Doe","specialization":"fish"}';
+    private CONST EXPECTED_GET_SLOTS_RESPONSE = '[{"id":1,"day":"2025-11-11","from_hour":"10.00","duration":15}]';
 
     public function test(): void
     {
-        $this->index();
+        $this->get_doctor_when_not_exist();
+        $this->create_new_doctor();
+        $this->get_doctor_when_exist();
+
+        $this->get_doctor_slots_when_no_slots();
+        $this->create_doctor_slot();
+        $this->get_doctor_slots();
     }
 
-    private function index(): void
+    private function get_doctor_when_not_exist(): void
     {
-        $expectedContent = '"ReallyDirty API v1.0"';
-        $this->client->request('GET', '/');
+        $this->requestDoctorId1();
+
+        $this->assertEquals('404', $this->client->getResponse()->getStatusCode());
+    }
+
+    private function get_doctor_when_exist(): void
+    {
+        $this->requestDoctorId1();
+
+        $response = $this->client->getResponse();
+
+        $this->assertEquals('200', $response->getStatusCode());
+        $this->assertEquals(self::EXPECTED_GET_RESPONSE, $response->getContent());
+    }
+
+    private function create_new_doctor(): void
+    {
+        $uri = '/doctor';
+        $body = [
+            'firstName' => 'John',
+            'lastName' => 'Doe',
+            'specialization' => 'fish'
+        ];
+
+        $this->postRequest($uri, $body);
+
+        $response = $this->client->getResponse();
+
+        $this->assertEquals('200', $response->getStatusCode());
+        $this->assertEquals(self::EXPECTED_CREATE_RESPONSE, $response->getContent());
+    }
+
+    private function get_doctor_slots_when_no_slots(): void
+    {
+        $this->requestDoctor1Slots();
 
         $response = $this->client->getResponse();
         $this->assertEquals('200', $response->getStatusCode());
-        $this->assertEquals($expectedContent, $response->getContent());
+        $this->assertEquals('[]', $response->getContent());
     }
 
+    private function get_doctor_slots(): void
+    {
+        $this->requestDoctor1Slots();
+
+        $response = $this->client->getResponse();
+        $this->assertEquals('200', $response->getStatusCode());
+        $this->assertEquals(self::EXPECTED_GET_SLOTS_RESPONSE, $response->getContent());
+    }
+
+    private function create_doctor_slot(): void
+    {
+        $uri = '/slots/1';
+        $body = [
+            'day' => '2025-11-11',
+            'duration' => '15',
+            'from_hour' => '10.00',
+        ];
+
+        $this->postRequest($uri, $body);
+
+        $response = $this->client->getResponse();
+        $this->assertEquals('200', $response->getStatusCode());
+    }
+
+
+    private function requestDoctorId1(): void
+    {
+        $this->client->request('GET', '/doctor?id=1');
+    }
+
+    private function requestDoctor1Slots(): void
+    {
+        $this->client->request('GET', '/slots/1');
+    }
 }
--- tests/E2ETestCase.php
+++ tests/E2ETestCase.php
@@ -11,12 +11,19 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 abstract class E2ETestCase extends WebTestCase
 {
+    protected array $entitiesToTruncate = [
+        DoctorEntity::class,
+        SlotEntity::class
+    ];
 
     protected KernelBrowser $client;
 
     protected function setUp(): void
     {
         $this->client = $this->createClient();
+        $this->truncateEntities(
+            $this->entitiesToTruncate
+        );
     }
 
     protected function postRequest(string $uri, array $body): void
@@ -31,4 +38,30 @@ abstract class E2ETestCase extends WebTestCase
 
     }
 
+    protected function serviceFromContainer(string $serviceId)
+    {
+        return static::$kernel->getContainer()->get($serviceId);
+    }
+
+    private function truncateEntities(array $entities)
+    {
+        $connection = $this->entityManager()->getConnection();
+        $databasePlatform = $connection->getDatabasePlatform();
+        if ($databasePlatform->supportsForeignKeyConstraints()) {
+            $connection->executeQuery('SET FOREIGN_KEY_CHECKS=0');
+        }
+        foreach ($entities as $entity) {
+            $query = $databasePlatform->getTruncateTableSQL(
+                $this->entityManager()->getClassMetadata($entity)->getTableName()
+            );
+            $connection->executeStatement($query);
+        }
+        if ($databasePlatform->supportsForeignKeyConstraints()) {
+            $connection->executeQuery('SET FOREIGN_KEY_CHECKS=1');
+        }
+    }
+    private function entityManager(): EntityManager
+    {
+        return $this->serviceFromContainer('doctrine')->getManager();
+    }
 }
